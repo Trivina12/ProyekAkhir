4a10a8387d14a3e3ac840109f6cdc586dae93bd2
AdrianAdidharma <adrianadidharma21@gmail.com>
2025-06-02 13:01:24 +0700

    Update Pemesanan.java

--- src/pemesananmakanan/Pemesanan.java
+++ src/pemesananmakanan/Pemesanan.java
@@ -1,40 +1,29 @@
 package pemesananmakanan;
-
+import java.util.HashMap;
 public class Pemesanan {
-    private String kode;
-    private String nama;
-    private int harga;
-    private int jumlah;
+    private HashMap<String, Makanan> daftarMakanan = new HashMap<>();
+    private HashMap<String, Minuman> daftarMinuman = new HashMap<>();
 
-    public Pemesanan(String kode, String nama, int harga, int jumlah) {
-        this.kode = kode;
-        this.nama = nama;
-        this.harga = harga;
-        this.jumlah = jumlah;
+    public Pemesanan() {
+        daftarMakanan.put("C1", new Makanan("C1", "Nasi Goreng", 30000));
+        daftarMakanan.put("C2", new Makanan("C2", "Mi Goreng", 28000));
+        daftarMakanan.put("C3", new Makanan("C3", "Mi Rebus", 28000));
+
+        daftarMinuman.put("D1", new Minuman("D1", "Jus Mangga", 15000));
+        daftarMinuman.put("D2", new Minuman("D2", "Air Mineral", 10000));
+        daftarMinuman.put("D3", new Minuman("D3", "Es Campur", 18000));
     }
 
-    public String getKode() {
-        return kode;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public int getHarga() {
-        return harga;
-    }
-
-    public int getJumlah() {
-        return jumlah;
-    }
-
-    public int getSubtotal() {
-        return harga * jumlah;
-    }
-
-    @Override
-    public String toString() {
-        return nama + " (" + kode + ") x " + jumlah + " = Rp" + getSubtotal();
+    public String prosesPesanan(String kode, int jumlah) {
+        if (daftarMakanan.containsKey(kode)) {
+            Makanan m = daftarMakanan.get(kode);
+            return jumlah + "x " + m.getNama() + " = Rp" + (jumlah * m.getHarga());
+        } else if (daftarMinuman.containsKey(kode)) {
+            Minuman m = daftarMinuman.get(kode);
+            return jumlah + "x " + m.getNama() + " = Rp" + (jumlah * m.getHarga());
+        } else {
+            return "Kode tidak ditemukan: " + kode;
+        }
     }
 }
+
